7ec4eff3b9e7c63020ec856e8763db501ed8ca81
js223kz <js223kz@student.lnu.se>
2016-04-26 10:37:08 +0200

    added app directory, server and routes file

---
+++ app/routes.js
@@ -0,0 +1,12 @@
+// expose the routes to our app with module.exports
+module.exports = function(app) {
+    
+    // routes ======================================================================
+
+    // application -------------------------------------------------------------
+    app.get('/', function(req, res) {
+        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+};
+
+
---
+++ app/server.js
@@ -0,0 +1,26 @@
+"use strict";
+// set up ========================
+let express  = require('express'),
+    app      = express(),
+    server     = require('http').createServer(app),
+    morgan = require('morgan'),           
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 8080;
+
+
+// configuration =================
+app.use(express.static(__dirname + '/public'));                 // set the static files location
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+// routes ======================================================================
+require('./routes.js')(app);
+
+require('dotenv').config()
+console.log(process.env.MY_API_KEY)
+
+// listen (start app with node server.js) ======================================
+server.listen(port);
+console.log("App listening on port 8080");
